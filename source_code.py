9afd08fabc591b8996ffa84ac00006c900f4a027
PRIYANSUPULAK <priyansu.pulak30@gmail.com>
2018-10-02 06:16:48 +0530

    updated

--- source_code.py
+++ source_code.py
@@ -40,7 +40,7 @@ def makeMove(board,letter,move):
 	board[move]=letter
 
 def isWinner(board,letter):
-	return ((board[7]==letter and board[8]==letter and board[9]==letter) or
+	return ((board[7]==letter and board[8]==letter and board[9]==letter)or
 		   (board[4]==letter and board[5]==letter and board[6]==letter) or
 		   (board[1]==letter and board[2]==letter and board[3]==letter) or
 		   (board[1]==letter and board[4]==letter and board[7]==letter) or		
@@ -50,11 +50,34 @@ def isWinner(board,letter):
 		   (board[7]==letter and board[5]==letter and board[3]==letter))
 
 
+def getBoardCopy(board):
+	dupboard=[]
+	for i in board:
+		dupboard.append(board[i])
+	return dupboard	
 
+def isSpaceFree(move,board):
+	return board[move]==" "	
 
+def getPlayerMove(board):
+	move=" "
+	while move not in "1 2 3 4 5 6 7 8 9".split() or not isSpaceFree(int(move),board):
+		print("what is your next move?(1-9)")
+		move=input() 
+	return int(move)	
 
+def RandomMoveFromList(board,movesList):
+	possibleMoves=[]
+	for i in movesList:
+		if isSpaceFree(i,board):
+			possibleMoves.append(i)
 
+	if len(possibleMoves)!=0:
+		return random.choice(possibleMoves)
+	else:
+		return None
 
+def getComputerMove()
 
 
 
